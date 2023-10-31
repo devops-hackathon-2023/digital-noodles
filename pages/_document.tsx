2b41ed7c05aec8e247302d451253f7a904cc2cca
Hugo Mrazek <hugo.mrazek@gmail.com>
2023-10-31 09:33:16 +0100

    Added modular dashboard library, added shadcn ui

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 
 export default function Document() {
   return (
