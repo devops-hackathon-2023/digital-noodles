84e6fc6d0fe7882e641f2f7ec4799e0447f76259
Hugo Mrazek <hugo.mrazek@gmail.com>
2023-10-31 18:01:16 +0100

    Added themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import {ThemeProvider} from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange>
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useTheme } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { setTheme } = useTheme()
   return (
     <>
       <Head>
@@ -15,6 +17,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <div onClick={() => setTheme("dark")}>Toggle dark theme</div>
+        <div onClick={() => setTheme("light")}>Toggle light theme</div>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
