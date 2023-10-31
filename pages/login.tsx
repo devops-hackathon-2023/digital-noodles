14114a8f0689584be933941a520a18e364a46b7f
Phuong Dong Cu <phuong.dong.cu@gmail.com>
2023-10-31 20:26:42 +0100

    added header and login page

---
+++ pages/login.tsx
@@ -0,0 +1,48 @@
+import {UserAuthForm} from "@/components/organisms/user-auth-form";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+import {buttonVariants} from "@/components/ui/button";
+import Image from "next/image";
+import collaboration from "../public/colab.png"
+import dopo from "../public/dopo_portal.png"
+
+const Login = () => {
+  return (
+    <div
+      className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link
+        href="/todo"
+        className={cn(
+          buttonVariants({variant: "ghost"}),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Login
+      </Link>
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 bg-zinc-900"/>
+        <div className="absolute top-1/2 left-1/2" style={{transform: "translate(-50%, -50%)"}}>
+          <Image src={dopo} alt={"devops portal"}/>
+        </div>
+        <div className="relative z-20 mt-auto flex justify-center">
+          <Image src={collaboration} alt={"s x digital|noodles"}/>
+        </div>
+      </div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+          <UserAuthForm/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/molecules/header.tsx
@@ -0,0 +1,31 @@
+import {Input} from "@/components/ui/input";
+import {ProfileDropdownMenu} from "@/components/organisms/profile-dropdown-menu";
+import {ModeToggle} from "@/components/ui/mode-toggle";
+import {usePathname} from "next/navigation";
+
+const Header = () => {
+  const pathname = usePathname()
+  const isLoginPage = pathname === '/login';
+
+  if (isLoginPage) {
+    return null;
+  }
+
+  return (
+    <header
+      className="sticky top-0 z-50 w-full border-b">
+      <div className="container flex h-14 items-center justify-between">
+        <strong>DOPO</strong>
+        <div className={"flex gap-4 items-center"}>
+          <Input placeholder={"Search..."} className={'w-full'}/>
+          <div className={"flex-shrink-0"}>
+            <ModeToggle/>
+          </div>
+          <ProfileDropdownMenu/>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
